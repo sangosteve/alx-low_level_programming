f00b95628fca31c0457cd550641afebac834710d
sangosteve <stevesango9@gmail.com>
2022-10-16 10:41:44 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Prints string using printf
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
